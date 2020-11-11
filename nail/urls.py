6aa112fdde5a4912b71f36221c54d0b35102db1b
harniy <forbedlam@gmail.com>
2020-11-12 00:52:36 +0300

    '12'

--- nail/urls.py
+++ nail/urls.py
@@ -6,7 +6,7 @@ from . import views
 urlpatterns = [
     path('', views.Home.as_view()),
     path('reservation', views.ReserationView.as_view(), name='reserv_form'),
-    path('<slug:slug>', views.Services.as_view(), name='services'),
+    path('<slug:slug>/', views.Services.as_view(), name='services'),
     path('page/booking', views.Booking, name='booking'),
     path('page/contacts', views.contacts, name='booking'),
     path('page/about', views.about),
